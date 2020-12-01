88691e6651a1990534f1dfd591d735907813a2d4
Randolph Marte <Randolphmarte@hotmail.com>
2020-12-01 10:29:30 -0400

    Ajustes del proyecto

--- Suromedica/Suromedica/Controllers/HomeController.cs
+++ Suromedica/Suromedica/Controllers/HomeController.cs
@@ -8,26 +8,18 @@ namespace Suromedica.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
         public ActionResult Home()
         {
             return View();
         }
-        public ActionResult About()
+        public ActionResult productosA()
         {
-            ViewBag.Message = "Your application description page.";
-
+            return View();
+        }
+        public ActionResult DetalleA1()
+        {
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
---
+++ Suromedica/Suromedica/Scripts/custom.js
@@ -0,0 +1,50 @@
+/******************************************
+    File Name: custom.js
+*******************************************/
+
+"use strict";
+
+/**== wow animation ==**/
+
+new WOW().init();
+
+/**== loader js ==*/
+
+$(window).load(function() {
+    $(".bg_load").fadeOut("slow");
+})
+
+
+
+	
+/** Casestudies Tab_bar js **/	
+	
+	$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
+
+
+	

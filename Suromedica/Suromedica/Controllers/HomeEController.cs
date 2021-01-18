234eef5974b893006faaa990e6bafc5018e975d1
randolph marte <randolphmarte@hotmail.com>
2021-01-18 13:02:20 -0400

    Version en English

---
+++ Suromedica/Suromedica/Controllers/HomeEController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Suromedica.Controllers
+{
+    public class HomeEController : Controller
+    {
+        // GET: HomeE
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult productosA()
+        {
+            return View();
+        }
+        public ActionResult productosB()
+        {
+            return View();
+        }
+        public ActionResult productosC()
+        {
+            return View();
+        }
+        public ActionResult DetalleA1()
+        {
+            return View();
+        }
+        public ActionResult DetalleA2()
+        {
+            return View();
+        }
+        public ActionResult DetalleA3()
+        {
+            return View();
+        }
+        public ActionResult DetalleA4()
+        {
+            return View();
+        }
+        public ActionResult DetalleA5()
+        {
+            return View();
+        }
+        public ActionResult DetalleA6()
+        {
+            return View();
+        }
+        public ActionResult DetalleB1()
+        {
+            return View();
+        }
+        public ActionResult DetalleB2()
+        {
+            return View();
+        }
+        public ActionResult DetalleB3()
+        {
+            return View();
+        }
+        public ActionResult DetalleC1()
+        {
+            return View();
+        }
+        public ActionResult DetalleC2()
+        {
+            return View();
+        }
+        public ActionResult DetalleC3()
+        {
+            return View();
+        }
+        public ActionResult DetalleC4()
+        {
+            return View();
+        }
+        public ActionResult DetalleC5()
+        {
+            return View();
+        }
+        public ActionResult DetalleC6()
+        {
+            return View();
+        }
+
+
+    }
+}
